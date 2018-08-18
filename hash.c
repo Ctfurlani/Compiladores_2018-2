0775422d9a7cf58c0f9d026847d4779499997ae4
Fokendov <38194317+Fokendov@users.noreply.github.com>
2018-08-18 13:58:54 -0300

    Add files via upload

---
+++ hash.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "hash.h"
+
+HASH_NODE*Table[HASH_SIZE]
+~
+void hashInit(void)
+{
+	int i;
+	for (i=0; i<HASH_SIZE; ++i)
+		Table[i]=0;	
+}
+
+int hashAddress(char *text)
+{
+	int i;
+	int address =1;
+	for (i=0; i<strlen(text); ++i)
+		address = (address*text[i])%HASH_SIZE +1
+	return address-1;
+}
+
+HASH_NODE* hashInsert (int type, char *text)
+{
+	HASH_NODE *newnode;
+	int address;
+	address = hasAddress(text);
+	if (newnode = hasFind(text))
+		return newnode;
+
+	newnode = (HASH_NODE*) calloc(1,sizeof(HASH_NODE));
+	newnode->type = type;	
+	newnode->text = calloc(strlen(text)+1,sizeof(char));
+	strcpy(newnode->text, text);
+	newnode->text = Table[address];
+	Table[address] = newnode;
+	return newnode;
+}
+
+HASH_NODE* hashFind(char *text)
+{
+	HASH_NODE *node;
+	int address;
+	address = hasAddress(text);
+	for(node=Table[address]; node; node = node->next)
+		if(!strcmp(text,node->text)
+			return node;
+	return 0;
+}
+
+void hashPrint(void);
+{
+	HASH_NODE *node;
+
+	int i;
+	for(i=0; i<HASH_SIZE; ++i)
+		for(node=table[i]; node; node = node->next)
+			fprintf(stderr, "Table[%d] has %s\n",i,node->text);
+}
