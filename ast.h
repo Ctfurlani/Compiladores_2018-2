d3d2bef14077bd29bbcf3bd22fcf795fa881c6f0
Fokendov <38194317+Fokendov@users.noreply.github.com>
2018-09-18 11:58:49 -0300

    Update ast.h

--- ast.h
+++ ast.h
@@ -1,8 +1,12 @@
-#define MAS_SONS 4
+#include hash.h
+
+#define MAX_SONS 4
+#define AST_SYMBOL 1
 typedef struct ast_node {
    int type;
    struct ast_node *son[MAX_SONS];
+   HASH_NODE *symbol;
 } AST;
 
-AST* astCreate(int type, AST* son0, AST* son1, AST* son2, AST* son3);
+AST* astCreate(int type, HASH_NODE* symbol, AST* son0, AST* son1, AST* son2, AST* son3);
 void astPrint(AST* node);
